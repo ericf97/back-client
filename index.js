c2e7f280b4697e7e9959540c021781e565e0d5ee
ericwest <fernandezeric1997@gmail.com>
2022-03-12 13:00:36 -0300

    enviroment

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const YAML = require('yamljs');
 const swaggerYml = YAML.load('./swagger.yml');
 const express = require('express');
 
+if(!process.env.DB_USER) {
+  require('dotenv').config();
+  console.log(process.env);
+}
+
 const app = express();
 
 const router = require('./router');
 
 app.listen(3000, () => {
-    console.log(`app listening port 3000`)
+  console.log(`app listening port 3000`)
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerYml));
