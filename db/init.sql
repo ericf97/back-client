b1916e5aa609f048546792de15175afe686aa9f4
ericwest <fernandezeric1997@gmail.com>
2022-06-28 00:14:29 -0300

    script db

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,12 @@
 Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
 
 create table users(
-    idUser bigint identity(1, 1) not null,
+    userId bigint identity(1, 1) not null,
     name nvarchar(255),
     lastName nvarchar (255),
     email nvarchar(255),
-    phone nvarchar(255)
+    phone nvarchar(255),
+    addressUser nvarchar(500)
 )
 
 drop table if exists states;
