8c8fb37340af296f16c7159fcbfdca26b71f0125
eric fernandez <ericfernandez@cripto-country.com>
2022-07-15 10:51:59 -0300

    rigth name

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ const errorHandler = require('../middleware/errror-handler.middle');
 const loginRouter = require('./login.router');
 const formRouter = require('./form.router');
 const userRouter = require('./user.router');
-const fileController = require('./file.router');
+const fileRouter = require('./file.router');
 
 router.use('/login', loginRouter);
 router.use('/cases', formRouter);
 router.use('/user', userRouter);
-router.use('/file', fileController);
+router.use('/file', fileRouter);
 
 router.use(errorHandler);
 
