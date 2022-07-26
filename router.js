8e5623c9dc7d1f74dd2bf565e74a15461d45f852
ewest <fernandezeric1997@gmail.com>
2022-07-26 14:41:41 -0300

    health

--- router.js
+++ router.js
@@ -4,5 +4,8 @@ const routes = require('./src/router');
 const router = Router();
 
 router.use('/api', routes);
+router.get('/health', (req, res) => {
+  res.status(200).send('Service Status ok');
+});
 
 module.exports = router;
